80750dcc405789abd37ff0f9ae57f285b5892cb1
Blazer <84545189+beforeblazergithub@users.noreply.github.com>
2023-04-04 18:12:51 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/uv/',
-    bare: 'https://youaresupercool.lol/bare/',
+    bare: 'ENTER BARE SERVER HERE',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
